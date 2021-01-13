b32fbf9e50c907e3c2eeb3c07f863dbab9184288
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-13 18:11:26 -0500

    Started Question 1, added class Node and class LinkedList

---
+++ Assignment2_Question1/LinkedList.h
@@ -0,0 +1,25 @@
+#ifndef LINKEDLIST
+#define LINKEDLIST
+
+#include "Node.h"
+
+template<typename T>
+class LinkedList
+{
+public:
+	LinkedList();
+	Node<T>* getHead() const;  // return the head pointer.
+	Node<T>* getTail() const;  // returns the tail pointer.
+	void ListAppend(T value);  // inserts an element at the end of the list.
+	void listPrepend(T value); //inserts a n element at the head of the list.
+	void insertAfter(Node<T>* curNode, T value); // Insert value after the curNode
+	void removeAfter(Node<T>* curNode); // remove Node after CurNode
+	void removeHead();  // Removes the first element.
+	void removeTail();  // removes the last element.
+	void printList() const;  // print the elements of the linked list.
+private:
+	Node<T>* head;
+	Node<T>* tail;
+};
+
+#endif // !1
---
+++ Assignment2_Question1/Node.h
@@ -0,0 +1,16 @@
+#ifndef NODE
+#define NODE
+
+
+
+template<typename T>
+class Node
+{
+public:
+	Node();
+	Node(T value, Node<T>* nextNode);
+	T data;
+	Node<T>* next;
+};
+
+#endif // !NODE
