19f5ff446451f2a581fd9696cf1ebaf372ecde79
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-13 19:00:17 -0500

    Implemented DoublyLinkedList class

--- Assignment2_Question2/DoublyLinkedList.cpp
+++ Assignment2_Question2/DoublyLinkedList.cpp
@@ -1,1 +1,64 @@
 #include "DoublyLinkedList.h"
+
+template<typename T>
+DoublyLinkedList<T>::DoublyLinkedList()
+{
+	this->head = nullptr;
+	this->tail = nullptr;
+}
+
+template<typename T>
+Node<T>* DoublyLinkedList<T>::getHead() const
+{
+	return this->head;
+}
+
+template<typename T>
+Node<T>* DoublyLinkedList<T>::getTail() const
+{
+	return this->tail;
+}
+
+template<typename T>
+void DoublyLinkedList<T>::listAppend(Node<T>* newNode)
+{
+	if (this->head == nullptr)
+	{
+		list->head = newNode;
+		list->tail = newNode;
+	}
+	else
+	{
+		this->tail->next = newNode;
+		newNode->prev = this->tail;
+		this->tail = newNode;
+	}
+}
+
+template<typename T>
+void DoublyLinkedList<T>::listPrepend(Node<T>* newNode)
+{
+	if (this->head == nullptr)
+	{
+		list->head = newNode;
+		list->tail = newNode;
+	}
+	else
+	{
+		newNode->next = list->head;
+		this->head->prev = newNode;
+		this->head = newNode;
+	}
+}
+
+template<typename T>
+void DoublyLinkedList<T>::printList() const
+{
+	Node<T> curNode = this->head;
+
+	while(curNode != nullptr)
+	{
+		cout << curNode;
+		curNode = curNode->next;
+	}
+}
--- Assignment2_Question2/DoublyLinkedList.h
+++ Assignment2_Question2/DoublyLinkedList.h
@@ -3,8 +3,21 @@
 
 #include "Node.h"
 
+template<typename T>
 class DoublyLinkedList
 {
+public:
+	DoublyLinkedList();
+	Node<T>* getHead() const;  // return the head pointer.
+	Node<T>* getTail() const;  // returns the tail pointer.
+	void listAppend(Node<T>* newNode);
+	void listPrepend(Node<T>* newNode);
+	void printList() const;
+
+private:
+	Node<T>* head;
+	Node<T>* tail;
+
 
 };
 
