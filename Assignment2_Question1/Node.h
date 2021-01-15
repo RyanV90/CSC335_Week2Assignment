2eedfc4fd6bd9193cc07d5067854abfe9ecdbefd
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-14 21:08:36 -0500

    Moved funtions in Node.cpp to Node.h

--- Assignment2_Question1/Node.h
+++ Assignment2_Question1/Node.h
@@ -18,3 +18,41 @@ private:
 };
 
 #endif // !NODE
+
+template<typename T>
+Node<T>::Node()
+{
+	this->nextNode = nullptr;
+	this->nodeData = 0;
+}
+
+template<typename T>
+Node<T>::Node(T nodeData, Node<T>* nextNode)
+{
+	this->nodeData = nodeData;
+	this->nextNode = nextNode;
+}
+
+template<typename T>
+T Node<T>::getNodeData() const
+{
+	return this->nodeData;
+}
+
+template<typename T>
+void Node<T>::setNodeData(T nodeData)
+{
+	this->nodeData = nodeData;
+}
+
+template<typename T>
+Node<T>* Node<T>::getNextNode() const
+{
+	return this->nextNode;
+}
+
+template<typename T>
+void Node<T>::setNextNode(Node<T>* nextNode)
+{
+	this->nextNode = nextNode;
+}
