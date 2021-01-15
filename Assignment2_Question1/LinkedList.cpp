c3ddfc27f4a7645109cac22b06fa419c2d0b3cba
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-14 20:28:59 -0500

    Defined class LinkedList

--- Assignment2_Question1/LinkedList.cpp
+++ Assignment2_Question1/LinkedList.cpp
@@ -1,1 +1,76 @@
 #include "LinkedList.h"
+
+template<typename T>
+LinkedList<T>::LinkedList()
+{
+
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::getHead() const
+{
+	return nullptr;
+}
+
+template<typename T>
+void LinkedList<T>::setHead(Node<T>* head)
+{
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::getTail() const
+{
+	return nullptr;
+}
+
+template<typename T>
+void LinkedList<T>::setTail(Node<T>* tail)
+{
+}
+
+template<typename T>
+void LinkedList<T>::listAppend(T nodeData)
+{
+}
+
+template<typename T>
+void LinkedList<T>::listPrepend(T nodeData)
+{
+}
+
+template<typename T>
+void LinkedList<T>::insertAfter(Node<T>* curNode, T nodeData)
+{
+}
+
+template<typename T>
+void LinkedList<T>::removeAfter(Node<T>* curNode)
+{
+}
+
+template<typename T>
+void LinkedList<T>::removeHead()
+{
+}
+
+template<typename T>
+void LinkedList<T>::removeTail()
+{
+}
+
+template<typename T>
+void LinkedList<T>::printList() const
+{
+}
+
+template<typename T>
+int LinkedList<T>::getLength() const
+{
+	return 0;
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::listSearch(T key)
+{
+	return nullptr;
+}
--- Assignment2_Question1/LinkedList.h
+++ Assignment2_Question1/LinkedList.h
@@ -5,7 +5,24 @@
 template<typename T>
 class LinkedList {
 
-
+public:
+	LinkedList();
+	Node<T>* getHead() const;
+	void setHead(Node<T>* head);
+	Node<T>* getTail() const;
+	void setTail(Node<T>* tail);
+	void listAppend(T nodeData);
+	void listPrepend(T nodeData);
+	void insertAfter(Node<T>* curNode, T nodeData);
+	void removeAfter(Node<T>* curNode); 
+	void removeHead();  
+	void removeTail();   
+	void printList() const; 
+	int getLength() const;
+	Node<T>* listSearch(T key);
+private:
+	Node<T>* head;
+	Node<T>* tail;
 };
 
 #endif // !LINKEDLIST
