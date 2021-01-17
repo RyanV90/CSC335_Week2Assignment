84e57ba4f91ee5cedf3ff1fc36c9e40a9c85907c
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-17 14:53:40 -0500

    Added some comments

--- Assignment2_Question2/DoublyLinkedListTester.cpp
+++ Assignment2_Question2/DoublyLinkedListTester.cpp
@@ -4,18 +4,22 @@ int main() {
 
 	DoublyLinkedList<int> list;
 
+	//Prepend some values to the list
 	list.listPrepend(1);
 	list.listPrepend(3);
 	list.listPrepend(5);
 	list.listPrepend(7);
 
+	//Print the list
 	list.printList();
 
+	//Append some values to the list
 	list.listAppend(2);
 	list.listAppend(4);
 	list.listAppend(6);
 	list.listAppend(8);
 
+	//Print the list
 	list.printList();
 
 	return 0;
