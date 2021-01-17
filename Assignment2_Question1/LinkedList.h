a19fc7f9660cdbea3f57e015c693d6c1813a6534
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-17 14:15:26 -0500

    Added comments to LinkedList.h

--- Assignment2_Question1/LinkedList.h
+++ Assignment2_Question1/LinkedList.h
@@ -3,6 +3,7 @@
 #include "Node.h"
 #include <iostream>
 
+//Creates a linked list object which uses nodes to store data
 template<typename T>
 class LinkedList {
 
@@ -28,6 +29,7 @@ private:
 
 #endif // !LINKEDLIST
 
+//Creates an empty linked list
 template<typename T>
 LinkedList<T>::LinkedList()
 {
@@ -35,39 +37,46 @@ LinkedList<T>::LinkedList()
 	this->tail = nullptr;
 }
 
+//Returns a pointer to the head node
 template<typename T>
 Node<T>* LinkedList<T>::getHead() const
 {
 	return this->head;
 }
 
+//Sets the pointer to a new head node
 template<typename T>
 void LinkedList<T>::setHead(Node<T>* head)
 {
 	this->head = head;
 }
 
+//Returns a pointer to the tail node
 template<typename T>
 Node<T>* LinkedList<T>::getTail() const
 {
 	return this->tail;
 }
 
+//Sets the pointer to a new tail node
 template<typename T>
 void LinkedList<T>::setTail(Node<T>* tail)
 {
 	this->tail = tail;
 }
 
+//Places a new node with data to the end of the linked list
 template<typename T>
 void LinkedList<T>::listAppend(T nodeData)
 {
 	Node<T>* newNode = new Node<T>(nodeData, nullptr);
 
+	//If the list is empty, set head and tail to new node
 	if (this->tail == nullptr)
 	{
 		this->head = this->tail = newNode;
 	}
+	//If the list contains nodes, add new node to end of list
 	else
 	{
 		this->tail->setNextNode(newNode);
@@ -75,15 +84,19 @@ void LinkedList<T>::listAppend(T nodeData)
 	}
 }
 
+//Places a new node with data to the beginning of the linked list
 template<typename T>
 void LinkedList<T>::listPrepend(T nodeData)
 {
 	Node<T>* tempNode = new Node<T>(nodeData, nullptr);
+	
+	//If the list is empty, set head and tail to new node
 	if (this->head == nullptr)
 	{
 		this->head = tempNode;
 		this->tail = tempNode;
 	}
+	//If the list contains nodes, add new node to beginning of list
 	else
 	{
 		tempNode->setNextNode(this->head);
@@ -91,6 +104,7 @@ void LinkedList<T>::listPrepend(T nodeData)
 	}
 }
 
+//Inserts a new node after the parameter node
 template<typename T>
 void LinkedList<T>::insertAfter(Node<T>* curNode, T nodeData)
 {
@@ -113,6 +127,7 @@ void LinkedList<T>::insertAfter(Node<T>* curNode, T nodeData)
 	}
 }
 
+//Removes the node after the parameter node
 template<typename T>
 void LinkedList<T>::removeAfter(Node<T>* curNode)
 {
@@ -128,6 +143,7 @@ void LinkedList<T>::removeAfter(Node<T>* curNode)
 	}
 }
 
+//Remove the current head node
 template<typename T>
 void LinkedList<T>::removeHead()
 {
@@ -139,6 +155,7 @@ void LinkedList<T>::removeHead()
 	}
 }
 
+//Remove the current tail node
 template<typename T>
 void LinkedList<T>::removeTail()
 {
@@ -155,6 +172,7 @@ void LinkedList<T>::removeTail()
 	this->tail = tempNode;
 }
 
+//Prints the data of each node in the linked list
 template<typename T>
 void LinkedList<T>::printList() const
 {
@@ -167,6 +185,7 @@ void LinkedList<T>::printList() const
 	std::cout << "\n";
 }
 
+//Returns the length of the linked list
 template<typename T>
 int LinkedList<T>::getLength() const
 {
@@ -182,13 +201,16 @@ int LinkedList<T>::getLength() const
 	return lengthOfListCounter;
 }
 
+//Searchs for a node with requested data and returns the pointer of that node.
 template<typename T>
 Node<T>* LinkedList<T>::listSearch(T key)
 {
 	Node<T>* curNode = this->head;
 
+	//While not at end of list
 	while (curNode != nullptr)
 	{
+		//If a node contains the data, return pointer to node
 		if (curNode->getNodeData() == key)
 		{
 			return curNode;
