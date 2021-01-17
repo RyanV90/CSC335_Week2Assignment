bb97256edf5a0dca68cd5550d027c05e7ec6ed06
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-17 14:51:23 -0500

    Added some comments

--- Assignment2_Question2/DoublyLinkedList.h
+++ Assignment2_Question2/DoublyLinkedList.h
@@ -3,6 +3,7 @@
 #include "Node.h"
 #include <iostream>
 
+//Creates a doubly linked list object which uses nodes to store data
 template<typename T>
 class DoublyLinkedList
 {
@@ -23,6 +24,7 @@ private:
 
 #endif // !DOUBLYLINKEDLIST
 
+//Creates an empty linked list
 template<typename T>
 inline DoublyLinkedList<T>::DoublyLinkedList()
 {
@@ -30,39 +32,46 @@ inline DoublyLinkedList<T>::DoublyLinkedList()
 	this->tail = nullptr;
 }
 
+//Returns the head of the linked list
 template<typename T>
 inline Node<T>* DoublyLinkedList<T>::getHead() const
 {
 	return this->head;
 }
 
+//Sets a new head of the linked list
 template<typename T>
 inline void DoublyLinkedList<T>::setHead(Node<T>* head)
 {
 	this->head = head;
 }
 
+//Returns the tail of the linked list
 template<typename T>
 inline Node<T>* DoublyLinkedList<T>::getTail() const
 {
 	return this->tail;
 }
 
+//Sets a new tail of the linked list
 template<typename T>
 inline void DoublyLinkedList<T>::setTail(Node<T>* tail)
 {
 	this->tail = tail;
 }
 
+//Places a new node with data to the end of the linked list
 template<typename T>
 inline void DoublyLinkedList<T>::listAppend(T nodeData)
 {
 	Node<T>* newNode = new Node<T>(nodeData, nullptr, nullptr);
 
+	//If the list is empty, set head and tail to new node
 	if (this->head == nullptr)
 	{
 		this->head = this->tail = newNode;
 	}
+	//If the list contains nodes, add new node to end of list
 	else
 	{
 		this->tail->setNextNode(newNode);
@@ -71,16 +80,19 @@ inline void DoublyLinkedList<T>::listAppend(T nodeData)
 	}
 }
 
+//Places a new node with data to the beginning of the linked list
 template<typename T>
 inline void DoublyLinkedList<T>::listPrepend(T nodeData)
 {
 	Node<T>* newNode = new Node<T>(nodeData, nullptr, nullptr);
 	
+	//If the list is empty, set head and tail to new node
 	if (this->head == nullptr)
 	{
 		this->head = newNode;
 		this->tail = newNode;
 	}
+	//If the list contains nodes, add new node to beginning of list
 	else
 	{
 		newNode->setNextNode(this->head);
@@ -89,11 +101,13 @@ inline void DoublyLinkedList<T>::listPrepend(T nodeData)
 	}
 }
 
+//Prints the data of each node in the linked list
 template<typename T>
 inline void DoublyLinkedList<T>::printList() const
 {
 	Node<T>* tempNode = this->head;
 	
+	//While not at end of list
 	while (tempNode != nullptr)
 	{
 		std::cout << tempNode->getNodeData() << " ";
