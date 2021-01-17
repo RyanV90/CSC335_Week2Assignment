5ea35a6970d166d9c2eca8b27232f4af612a0c02
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-17 14:24:01 -0500

    Added some comments. Added error handling for searching a null pointer

--- Assignment2_Question1/LinkedListFunctionTest.cpp
+++ Assignment2_Question1/LinkedListFunctionTest.cpp
@@ -4,30 +4,41 @@ int main() {
 
 	LinkedList<int> list;
 
+	//Append two values to list
 	list.listAppend(5);
 	list.listAppend(6);
 	
+	//Print length of list and print list
 	std::cout << "Length of list: " << list.getLength() << std::endl;
 	list.printList();
 
+	//Prepend two values to list
 	list.listPrepend(1);
 	list.listPrepend(2);
 	
+	//Print length of list and print list
 	std::cout << "Length of list: " << list.getLength() << std::endl;
 	list.printList();
 
-	//list.insertAfter(list.getHead()->getNextNode()->getNextNode(), 9);
+	//Removes the node after the head
 	list.removeAfter(list.getHead());
 
+	//Print length of list and print list
 	std::cout << "Length of list: " << list.getLength() << std::endl;
 	list.printList();
 
-	list.insertAfter(list.listSearch(5), 7);
-
-	std::cout << "Length of list: " << list.getLength() << std::endl;
-	list.printList();
-
-	std::cout << list.listSearch(50);
+	//If the node data is found, insert a new node after it.
+	if (list.listSearch(5))
+	{
+		list.insertAfter(list.listSearch(5), 7);
+		std::cout << "Length of list: " << list.getLength() << std::endl;
+		list.printList();
+	}
+	//If the node is not found, do not try to insert.
+	else
+	{
+		std::cout << "The requested node was not found in the list." << std::endl;
+	}
 
 	return 0;
 }
