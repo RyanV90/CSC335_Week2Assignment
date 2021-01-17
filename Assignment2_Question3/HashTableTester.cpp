ae7bb12dbc7573a148abea065fe628a61b6727c3
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-17 15:19:22 -0500

    Added some comments and output notes

--- Assignment2_Question3/HashTableTester.cpp
+++ Assignment2_Question3/HashTableTester.cpp
@@ -1,7 +1,6 @@
 #include "HashTable.h"
-#include <algorithm>
 
-// Generates random integer from 0 to maxValue
+//Generates random integer from 0 to maxValue
 int generateRandomNumber(int maxValue) {
 
 	int randomNumber = rand() % maxValue + 1;
@@ -10,40 +9,57 @@ int generateRandomNumber(int maxValue) {
 }
 
 int main() {
-	int listSize = 20;
+	
+	int integerListSize = 20; //Size of the list of integers being inserted into hash table
 
-	//vector<int> vect(31, -1);
-	vector<int> listOfRandomIntegers(listSize, 0);
-	HashTable<int> table1;
-	HashTable<int> table2;
+	vector<int> listOfRandomIntegers(integerListSize, 0); //List of integers being inserted into hash table
+	HashTable<int> linearProbingHashTable; //Hash table to hash by linear probing
+	HashTable<int> quadraticProbingHashTable; //Hash table to hash by quadratic probing
 
 	srand(time(0));
-	for (int i = 0; i < listSize; i++)
+	//Generate list of random integers from 0 to 100 of integerListSize;
+	for (int i = 0; i < integerListSize; i++)
 	{
 		listOfRandomIntegers.at(i) = generateRandomNumber(100);
 	}
-	sort(listOfRandomIntegers.begin(), listOfRandomIntegers.end());
 
-	for (int i = 0; i < listSize; i++)
+	//Inserts list of integers into a hash table using linear probing.
+	for (int i = 0; i < integerListSize; i++)
 	{
-		table1.hashInsertLprobe(listOfRandomIntegers.at(i));
+		linearProbingHashTable.hashInsertLprobe(listOfRandomIntegers.at(i));
 	}
 
-	for (int i = 0; i < listSize; i++)
+	//Inserts list of integers into a hash table using quadratic probing.
+	for (int i = 0; i < integerListSize; i++)
 	{
-		table2.hashInsertQ(listOfRandomIntegers.at(i));
+		quadraticProbingHashTable.hashInsertQ(listOfRandomIntegers.at(i));
 	}
 
+	//Prints the list of integers being inserting into hash tables
+	cout << "Number List: ";
 	for (int number : listOfRandomIntegers)
 	{
-		cout << number << ", ";
+		cout << number << " ";
 	}
 	cout << endl;
 
-	table1.printHashTable();
-	cout << "Collisons: " << table1.getCollisionCounter() << endl;
-	table2.printHashTable();
-	cout << "Collisons: " << table2.getCollisionCounter() << endl;
+	//Print the table size, number of values, total number of collisions, and final hash table of linear probing hash
+	cout << "\nHash table inserting by linear probing" << "\nTable Size: " << linearProbingHashTable.getTableSize() << 
+		"\nNumber of values being inserted: " << integerListSize << "\nTotal number of collisions: " << linearProbingHashTable.getCollisionCounter() << endl;
+	linearProbingHashTable.printHashTable();
+	
+	//Prints the list of integers being inserting into hash tables
+	cout << "\nNumber List: ";
+	for (int number : listOfRandomIntegers)
+	{
+		cout << number << " ";
+	}
+	cout << endl;
+	
+	//Print the table size, number of values, total number of collisions, and final hash table of quadratic probing hash
+	cout << "\nHash table inserting by linear probing" << "\nTable Size: " << quadraticProbingHashTable.getTableSize() <<
+		"\nNumber of values being inserted: " << integerListSize << "\nTotal number of collisions: " << quadraticProbingHashTable.getCollisionCounter() << endl;
+	quadraticProbingHashTable.printHashTable();
 
 	return 0;
 }
